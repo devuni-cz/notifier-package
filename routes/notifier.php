d54385b3c989a7ffa5394ff5d5aa36d9030cdd74
David Janeček <davidjanecek123@seznam.cz>
2025-08-21 13:28:59 +0200

    Chore: throttle interval

--- routes/notifier.php
+++ routes/notifier.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Devuni\Notifier\Controllers\NotifierController;
 
 Route::get('/api/backup', NotifierController::class)
-    ->middleware('throttle:5,1');
+    ->middleware('throttle:5,60');
